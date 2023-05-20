4e2faa67edb774deb14da8e196fe0f02b7230139
Janeth_Guadalupe_Mora_Aviña <janethaa27@gmail.com>
2023-05-19 21:58:16 -0600

    agregar evidencia entrega 1

---
+++ Entrega1/Ejercicio3/Ejercicio_3_consigna_grupo_1_12-05-2023.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir DH
+mkdir DH/Películas
+mkdir DH/Películas/Infantiles DH/Películas/Accion DH/Películas/Terror DH/Películas/Comedia DH/Películas/Romanticas
+touch DH/Películas/Infantiles/Intenzamente.txt DH/Películas/Infantiles/"El juego del miedo.xls" DH/Películas/Infantiles/"Rapido y furioso.pdf" DH/Películas/Infantiles/Titanic.jpg
+touch DH/Películas/Accion/"Smooth criminal.txt" DH/Películas/Accion/"Diario de una pasion.js" DH/Películas/Accion/"El hexorcista.xls" DH/Películas/Accion/Cars.jpg
+touch DH/Películas/Terror/"La dama y el bagavundo.png" DH/Películas/Terror/"Yo antes de ti.xls" DH/Películas/Terror/"Que paso ayer.pdf" DH/Películas/Terror/"We will rock you.js"
+touch DH/Películas/Comedia/Madagaskar.js DH/Películas/Comedia/"Orgullo y prejuicio.txt" DH/Películas/Comedia/IT.xls DH/Películas/Comedia/"Mision imposible.png"
+touch DH/Películas/Romanticas/Pocahontas.jpg DH/Películas/Romanticas/Annabelle.js DH/Películas/Romanticas/"Los vengadores.js" DH/Películas/Romanticas/"La mascara.js"
